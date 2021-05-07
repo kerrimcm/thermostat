785ea3e0d73e09fc84891e9763995dd83a618205
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-07 18:06:31 +0100

    Added weather API

--- thermostatInterface.js
+++ thermostatInterface.js
@@ -33,4 +33,23 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#power-saving-status').innerText = 'off';
     updateTemperature();
   });
+
+  const displayWeather = (city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+  
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
+  }
+
+  displayWeather('London');
+    document.querySelector('#select-city').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = document.querySelector('#current-city').value;
+    displayWeather(city);
+  })
 });
