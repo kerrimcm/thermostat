275b56978a399175c03422716ac446431fbf691e
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-07 11:36:12 +0100

    Temperature can go up

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -10,4 +10,9 @@ describe ('Thermostat', () => {
   it('starts at 20 degrees', function() {
     expect(thermostat.getCurrentTemp()).toEqual(20);
   });
+
+  it('increases the temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemp()).toEqual(21);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -8,4 +8,8 @@ class Thermostat {
   getCurrentTemp() {
     return this.temperature;
   }
+
+  up() {
+    this.temperature++
+  }
 };
